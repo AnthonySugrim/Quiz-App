d680473e963669480eccb28b1b0f078fc2832119
Anthony Sugrim <anthonysugrim16@gmail.com>
2021-08-14 12:43:43 -0400

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Place the OPEN box on the counter.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Adjective"
+  },
+  {
+    title: "Jeremy OPENED the door.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Verb"
+  },  {
+    title: "The OPENING is next week.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Noun"
+  },  {
+    title: "Later, we will go to the DANCE recital.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Adjective"
+  },  {
+    title: "Will you DANCE with me?",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Verb"
+  },  {
+    title: "The DANCE ends at ten.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Noun"
+  },  {
+    title: "Lydia climbed the PLAY structure at the park.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Adjective"
+  },  {
+    title: "The children PLAY outside in the summer.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Verb"
+  },  {
+    title: "Norah invited you to her PLAY.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Noun"
+  },  {
+    title: "Pack your WASHED clothes away.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Adjective"
+  },  {
+    title: "Dad WASHED the car last Saturday.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Verb"
+  },  {
+    title: "All dirty clothes go to WASH.",
+    choices: ["Noun", "Adjective", "Verb", "None Of the Above"],
+    answer: "Noun"
+  }
+];
